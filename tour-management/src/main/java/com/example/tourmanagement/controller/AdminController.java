ed88e2211a3e5fdc0bce4b87853b68912faad8d6
Thang <107343290+ThangBuiChien@users.noreply.github.com>
2024-05-23 14:53:17 +0700

    show email when login in dropdown button

--- tour-management/src/main/java/com/example/tourmanagement/controller/AdminController.java
+++ tour-management/src/main/java/com/example/tourmanagement/controller/AdminController.java
@@ -1,15 +1,21 @@
 package com.example.tourmanagement.controller;
 
 import com.example.tourmanagement.model.Tour;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.example.tourmanagement.service.TourService;
 
@@ -23,7 +29,14 @@ public class AdminController {
     }
 
     @GetMapping({"/", ""})
-    public String viewHomePage(Model model){
+    public String viewHomePage(Model model,
+                               @AuthenticationPrincipal UserDetails userDetails,
+                               HttpSession session){
+
+        if(userDetails != null){
+            session.setAttribute("email", userDetails.getUsername());
+
+        }
 
 
         return "index";
